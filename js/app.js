6a7231aba15c36b69040f5951598b4048670beef
Victor Reginaldo <156149542+Victor-Reginaldo@users.noreply.github.com>
2024-02-12 15:08:30 -0300

    OK
    Iniciando o repositório

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function alterarStatus (id) {
+    let  jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        let confirmacao = confirm('Você realmente deseja devolver este jogo?')
+
+        if(confirmacao) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }        
+    }else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver'
+    }
+}
